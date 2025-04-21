b3af78e32ce040d848c432dbde815945db7f2469
knkn9393 <kkn52582@gmail.com>
2025-04-21 03:17:55 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/maqamat/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.maqamat;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button playBayat = findViewById(R.id.btn_play_bayat);
+        playBayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bayat_scale);
+                mediaPlayer.start();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
